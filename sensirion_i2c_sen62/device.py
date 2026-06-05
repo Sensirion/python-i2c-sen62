e61de10ff8797c272370b45a0b918c975a318742
Driver Generator 2 <drivergenerator2@sensirion.com>
2026-06-05 11:21:51 +0000

    Generate SEN62 driver from SEN62 model version 1.2.1

--- sensirion_i2c_sen62/device.py
+++ sensirion_i2c_sen62/device.py
@@ -7,7 +7,7 @@
 #
 # Generator:     sensirion-driver-generator 1.7.0
 # Product:       sen62
-# Model-Version: 1.2.0
+# Model-Version: 1.2.1
 #
 """
 The class Sen62DeviceBase implements the low level interface of the sensor.
